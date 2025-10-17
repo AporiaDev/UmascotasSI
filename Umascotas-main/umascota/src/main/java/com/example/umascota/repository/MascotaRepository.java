6544c1ff44384f66321258855dbb8697d923c84a
Reinaldo Cárdenas Tirado <110806772+reinaldocardenas23@users.noreply.github.com>
2025-10-17 17:19:50 -0500

    ggs

--- Umascotas-main/umascota/src/main/java/com/example/umascota/repository/MascotaRepository.java
+++ Umascotas-main/umascota/src/main/java/com/example/umascota/repository/MascotaRepository.java
@@ -1,6 +1,7 @@
 package com.example.umascota.repository;
 
 import com.example.umascota.model.Mascota;
+import com.example.umascota.model.Mascota.EstadoPublicacion;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MascotaRepository extends JpaRepository<Mascota, Long> {
     List<Mascota> findByNombreIgnoreCase(String nombre);
+    List<Mascota> findByEstadoPublicacion(EstadoPublicacion estadoPublicacion);
 }
 
