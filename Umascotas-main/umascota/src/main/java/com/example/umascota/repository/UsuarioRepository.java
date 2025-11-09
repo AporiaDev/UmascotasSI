6eecfaf0cb1869c0b3f67c39adf62bfb31e27305
FrescoDev <silbacrack@gmail.com>
2025-11-09 16:21:33 -0500

    Add files via upload

--- Umascotas-main/umascota/src/main/java/com/example/umascota/repository/UsuarioRepository.java
+++ Umascotas-main/umascota/src/main/java/com/example/umascota/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package com.example.umascota.repository;
 
-import com.example.umascota.model.Usuario;
+
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.umascota.model.usuario.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByCorreoElectronico(String correoElectronico);
+    Optional<Usuario> findByIdUsuario(Long IdUsuario);
 }
