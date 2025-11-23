cfcce9459d41642a046514d7de0e723e0dad598e
crisjohnson1 <132920546+crisjohnson1@users.noreply.github.com>
2025-11-23 18:27:26 -0500

    Update Umascota2Application.java
    q

--- Umascotas-main/umascota/src/main/java/com/example/umascota/Umascota2Application.java
+++ Umascotas-main/umascota/src/main/java/com/example/umascota/Umascota2Application.java
@@ -2,17 +2,12 @@ package com.example.umascota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.umascota")
 public class Umascota2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Umascota2Application.class, args);
-	}
-
-}
-@SpringBootApplication(scanBasePackages = "com.example.umascota")
-public class Umascota2Application {
     public static void main(String[] args) {
         SpringApplication.run(Umascota2Application.class, args);
     }
